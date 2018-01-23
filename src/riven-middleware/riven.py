8a4da7f97a3cb8fbcfa0927421ce5074e00a7afd
metis <metis@metiss-MacBook-Pro.local>
2018-01-22 21:32:49 -0500

    Many updates

--- src/riven-middleware/riven.py
+++ src/riven-middleware/riven.py
@@ -1,18 +1,64 @@
-import docker, time
+import docker, time, json, datetime, sched, threading
 
 class Riven:
     def __init__(self):
+        self.boxes = json.load(open("config/boxes.json"))
+        self.config = json.load(open("config/config.json"))
         self.client = docker.from_env()
-        self.nodes=[]
+        self.nodes={}
+        self.t = threading.Thread(target=self.schedule_node_check, args = ())
+        self.s = sched.scheduler(time.time, time.sleep)
+        self.t.daemon = True
+        self.t.start()
+        #self.schedule_node_check()
+        #s.enter(10, 1, node_check)
+        #s.run()
 
-    def new_node(self):
-        port = self.find_new_node_port()
-        self.nodes.append(port)
-        container = self.client.containers.run("boxbox-node:180116", ports={'3000/tcp': port}, detach=True, name="boxbox-node-{}".format(port))
-        time.sleep(1) # This is ghetto and I want to replace it with promises. Promise.
-        return port
+    def schedule_node_check(self):
+        print "sched"
+        self.s.enter(5, 1, self.node_check, ())
+        self.s.run()
+        
+    def node_check(self):
+        print "check"
+        for node in self.nodes:
+            if datetime.datetime.strptime(self.nodes[node]["last_seen"]) < datetime.datetime.now()-datetime.timedelta(seconds=30):
+                self.kill_node(node)
+        self.schedule_node_check()
 
+            #if lastplus.date < datetime.datetime.now()-datetime.timedelta(seconds=20):
+        
+    def is_awake(self, box):
+        if box in self.nodes:
+            self.nodes[box]["last_seen"] = str(datetime.datetime.now().time())
+            print self.nodes[box]
+        
+    def new_node(self, box):
+        if box in self.boxes:
+            port = self.find_new_node_port()
+            node_id = "{}{}".format(self.boxes[box]["container"], port)
+            box_info = {"{}".format(node_id):{"port" : port, "last_seen" : datetime.datetime.now().time()}}
+            print box_info
+            self.nodes.update(box_info)
+            container = self.client.containers.run("boxbox-node:180116", ports={'3000/tcp': port}, detach=True, name="boxbox-node-{}".format(port))
+            loaded = self.wait_for_start_log(container, self.boxes[box]["start_log"], self.config["wait_load_time"])
+            if loaded is "True":
+                return node_id
+            else:
+                print "Load failed"
+                return 0
+        else:
+            return 0
 
+    def wait_for_start_log(self, container, start_log, wait_time):
+        while start_log not in container.logs():
+            wait_time = wait_time - 1
+            print "Waiting on Container Load... {}".format(wait_time)
+            time.sleep(1)
+            if wait_time < 1:
+                return "False"
+        return "True"
+    
     def find_new_node_port(self):
         if self.nodes:
             return max(self.nodes)+1
@@ -20,4 +66,4 @@ class Riven:
             return 3000
         
     def kill_node(self,node):
-        pass
+        print node
--- src/riven-middleware/server.py
+++ src/riven-middleware/server.py
@@ -1,20 +1,41 @@
 from flask import Flask, render_template, request, redirect
-import os
+import os, json, atexit
+from flask_socketio import SocketIO
 
 try:  
     app = Flask(__name__)
     from riven import Riven
-
     r = Riven()
+    s = sched.schedule(time.time, time.sleep)
+    app.config['SECRET_KEY'] = 'secret!'
+    socketio = SocketIO(app)
 
-    @app.route('/')
-    def index():
-        port = r.new_node()
-        return redirect("http://localhost:{}".format(port), code=302)
+    @app.route("/<box>")
+    def index(box):
+        node_id = r.new_node(box)
+        port = r.nodes[node_id]["port"]
+        #port = r.new_node(box)
+        if port is not 0:
+            direct = "http://localhost:{}".format(port)
+            return render_template(r.boxes[box]["template"], direct=direct, user_info = r.boxes[box]["user_info"], node_id = node_id)
+        else:
+            return "Could not load {}".format(box)
 
-except:
-    # In case of emergency, panic
+    @socketio.on('awake')
+    def is_awake(json):
+        r.is_awake(json["data"])
+        
+
+except Exception as e:
+    print e
+
+@atexit.register
+def kill_containers():
     os.system('docker stop $(docker ps -a -q)')
     os.system('docker rm $(docker ps -a -q)')
+
+
+if __name__ == '__main__':
+    socketio.run(app)
     
 
